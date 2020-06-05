14a8b678d9ad472b5c8fa6542064903062f96b43
Alex Penaloza <alexdaniel2601@gmail.com>
2020-06-05 07:35:15 -0400

    se agrega layout y seccion de tableros melaminicos listo con nuevo diseño

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Modular Top</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,900|Oswald:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    
+  </head>
+
+
+
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+  
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+   
+   
+    <header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
+
+      <div class="container">
+        <div class="row align-items-center">
+          
+          <div class="col-6 col-xl-2">
+            
+            <h1 class="mb-0 site-logo m-0 p-0">
+              
+              <a href="index.html" class="mb-0">
+                    <img src="images/logo_blanco_web.png" alt="logo">
+                </a>
+
+            </h1>
+            <!-- Original de la plantilla
+            <h1 class="mb-0 site-logo m-0 p-0"><a href="index.html" class="mb-0">Warehouse</a></h1>
+            -->
+          </div>
+  
+
+          <div class="col-12 col-md-10 d-none d-xl-block">
+            <nav class="site-navigation position-relative text-right" role="navigation">
+
+              <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                <li><a href="#home-section" class="nav-link">Inicio</a></li>
+                <li><a href="#properties-section" class="nav-link">Materia Prima</a></li>
+                <li><a href="#about-section" class="nav-link">Fabricacion</a></li>
+                <li><a href="#services-section" class="nav-link">Servicios</a></li>
+                <li><a href="#modular-top" class="nav-link">Modular Top</a></li>
+                <li><a href="#news-section" class="nav-link">Novedades</a></li>
+                <li><a href="#contact-section" class="nav-link">Contactanos</a></li>
+              </ul>
+            </nav>
+          </div>
+
+
+          <div class="col-6 d-inline-block d-xl-none ml-md-0 py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-white float-right"><span class="icon-menu h3"></span></a></div>
+
+        </div>
+      </div>
+      
+    </header>
+
+    @yield('content')
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.fancybox.min.js"></script>
+  <script src="js/jquery.sticky.js"></script>
+  
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,90 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Modular Top</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('layouts.layout');
 
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,900|Oswald:300,400,700" rel="stylesheet">
-    <link rel="stylesheet" href="fonts/icomoon/style.css">
+@section('content')
 
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/jquery-ui.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-
-    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
-
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-
-    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
-
-    <link rel="stylesheet" href="css/aos.css">
-
-    <link rel="stylesheet" href="css/style.css">
-    
-  </head>
-
-
-
-  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
-  
-  <div class="site-wrap">
-
-    <div class="site-mobile-menu site-navbar-target">
-      <div class="site-mobile-menu-header">
-        <div class="site-mobile-menu-close mt-3">
-          <span class="icon-close2 js-menu-toggle"></span>
-        </div>
-      </div>
-      <div class="site-mobile-menu-body"></div>
-    </div>
    
     
-    <header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
-
-      <div class="container">
-        <div class="row align-items-center">
-          
-          <div class="col-6 col-xl-2">
-            
-            <h1 class="mb-0 site-logo m-0 p-0">
-              
-              <a href="index.html" class="mb-0">
-                    <img src="images/logo_blanco_web.png" alt="logo">
-                </a>
-
-            </h1>
-            <!-- Original de la plantilla
-            <h1 class="mb-0 site-logo m-0 p-0"><a href="index.html" class="mb-0">Warehouse</a></h1>
-            -->
-          </div>
-  
-
-          <div class="col-12 col-md-10 d-none d-xl-block">
-            <nav class="site-navigation position-relative text-right" role="navigation">
-
-              <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
-                <li><a href="#home-section" class="nav-link">Inicio</a></li>
-                <li><a href="#properties-section" class="nav-link">Materia Prima</a></li>
-                <li><a href="#about-section" class="nav-link">Fabricacion</a></li>
-                <li><a href="#services-section" class="nav-link">Servicios</a></li>
-                <li><a href="#modular-top" class="nav-link">Modular Top</a></li>
-                <li><a href="#news-section" class="nav-link">Novedades</a></li>
-                <li><a href="#contact-section" class="nav-link">Contactanos</a></li>
-              </ul>
-            </nav>
-          </div>
-
-
-          <div class="col-6 d-inline-block d-xl-none ml-md-0 py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-white float-right"><span class="icon-menu h3"></span></a></div>
-
-        </div>
-      </div>
-      
-    </header>
-
     
     <div class="site-block-wrap">
     <div class="owl-carousel with-dots">
@@ -137,101 +56,108 @@
   <!-- Fin seccion head-->  
 
   <!-- Seccion Materia Prima-->
-  <div class="site-section" id="properties-section">
+  <section class="py-5 bg-brown site-section how-it-works" id="howitworks-section">
       <div class="container">
 
-        <div class="row mb-5">
-          <div class="col-md-7 text-left">
-            <h2 class="section-title mb-3">Tableros Melaminicos</h2>
+      <div class="contenedor">
+
+          <div class="titulotm">
+            <h2 class="section-title2">Tableros Melaminicos</h2>
+            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus minima neque tempora reiciendis.</p>
             <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus minima neque tempora reiciendis.</p>
           </div>
-        </div>
-
-        <div class="row large-gutters">
-          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
-            <div class="ftco-media-1">
-              <div class="ftco-media-1-inner">
-                <a href="" class="d-inline-block mb-4"><img src="images/tableros/sunny-white-high-gloss.jpg" alt="FImageo" class="img-fluid"></a>
-                <div class="ftco-media-details">
-                  <h3>Para interiores</h3>
-                  <p>Nacionales - Importados</p>
-                  
-                  <!--<strong>$20,000,000</strong>-->
-                </div>
-  
-              </div> 
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
-              <div class="ftco-media-1">
-                  <div class="ftco-media-1-inner">
-                    <a href="" class="d-inline-block mb-4"><img src="images/tableros/alentejo-elm-high-gloss.jpg" alt="Image" class="img-fluid"></a>
-                    <div class="ftco-media-details">
-                      <h3>Para acabados</h3>
-                      <p>Alto Brillo - Importados</p>
-                     
-                    </div>
-      
-                  </div> 
-                </div>
-          </div>
-          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
-              <div class="ftco-media-1">
-                  <div class="ftco-media-1-inner">
-                    <a href="" class="d-inline-block mb-4"><img src="images/tableros/lava-grey-super-mate.jpg" alt="Image" class="img-fluid"></a>
-                    <div class="ftco-media-details">
-                      <h3>Para Acabados</h3>
-                      <p>Super Mate - Importados</p>
-                     
-                    </div>
-      
-                  </div> 
-                </div>
-          </div>
           
-          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
+          <div class="itemchildtm">
+            <!-- cuadro 1 -->
+            <div class="hijostm">
               <div class="ftco-media-1">
                 <div class="ftco-media-1-inner">
-                  <a href="" class="d-inline-block mb-4"><img src="images/tableros/brillo_doblecara.png" alt="Image" class="img-fluid"></a>
+                  <a href="" class=""><img src="images/tableros/alentejo-elm-high-gloss.jpg" alt="FImageo" class="img-tm"></a>
                   <div class="ftco-media-details">
-                      <h3>Cocina Tablero Oneskin.</h3>
-                      <p>Caracas - Venezuela</p>
+                    <h3>Para interiores</h3>
+                    <p>Nacionales - Importados</p>
+                    <!--<strong>$20,000,000</strong>-->
                   </div>
-    
                 </div> 
               </div>
             </div>
-            <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
-                <div class="ftco-media-1">
-                    <div class="ftco-media-1-inner">
-                      <a href="" class="d-inline-block mb-4"><img src="images/tableros/1_prime-1280.jpg" alt="Image" class="img-fluid"></a>
-                      <div class="ftco-media-details">
-                          <h3>Mobiliarios Hogar</h3>
-                          <p>LA Guaira - Venezuela</p>
-                      </div>
-        
-                    </div> 
+            
+            <!-- cuadro 2 -->
+            <div class="hijostm">
+              <div class="ftco-media-1">
+                <div class="ftco-media-1-inner">
+                  <a href="" class=""><img src="images/tableros/grey-oak-super-mate.jpg" alt="FImageo" class="img-tm"></a>
+                  <div class="ftco-media-details">
+                      <h3>Para acabados</h3>
+                      <p>Alto Brillo - Importados</p>
+                    <!--<strong>$20,000,000</strong>-->
                   </div>
-            </div>
-            <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
-                <div class="ftco-media-1">
-                    <div class="ftco-media-1-inner">
-                      <a href="" class="d-inline-block mb-4"><img src="images/tableros/1_cozinha_supermatte_grande.jpg" alt="Image" class="img-fluid"></a>
-                      <div class="ftco-media-details">
-                          <h3>Mobiliario Oficina.</h3>
-                          <p>Caracas - Venezuela</p>
-                      </div>
-        
-                    </div> 
-                  </div>
+                </div> 
+              </div>
             </div>
             
+            <!-- cuadro 3 -->
+            <div class="hijostm">
+              <div class="ftco-media-1">
+                <div class="ftco-media-1-inner">
+                  <a href="" class=""><img src="images/tableros/sunny-white-high-gloss.jpg" alt="FImageo" class="img-tm"></a>
+                  <div class="ftco-media-details">
+                      <h3>Para Acabados</h3>
+                      <p>Super Mate - Importados</p>
+                    <!--<strong>$20,000,000</strong>-->
+                  </div>
+                </div> 
+              </div>
+            </div>
+            
+            <!-- cuadro 4 -->
+            <div class="hijostm">
+              <div class="ftco-media-1">
+                <div class="ftco-media-1-inner">
+                  <a href="" class=""><img src="images/tableros/1_prime-1280.jpg" alt="FImageo" class="img-tm"></a>
+                  <div class="ftco-media-details">
+                      <h3>Cocina Tablero Oneskin.</h3>
+                      <p>Caracas - Venezuela</p>
+                    <!--<strong>$20,000,000</strong>-->
+                  </div>
+                </div> 
+              </div>
+            </div>
+            
+            <!-- cuadro 5 -->
+            <div class="hijostm">
+              <div class="ftco-media-1">
+                <div class="ftco-media-1-inner">
+                  <a href="" class=""><img src="images/tableros/1_cozinha_supermatte_grande.jpg" alt="FImageo" class="img-tm"></a>
+                  <div class="ftco-media-details">
+                          <h3>Mobiliarios Hogar</h3>
+                          <p>LA Guaira - Venezuela</p>
+                    <!--<strong>$20,000,000</strong>-->
+                  </div>
+                </div> 
+              </div>
+            </div>
+            
+            <!-- cuadro 6 -->
+            <div class="hijostm">
+              <div class="ftco-media-1">
+                <div class="ftco-media-1-inner">
+                  <a href="" class=""><img src="images/tableros/brillo_doblecara.png" alt="FImageo" class="img-tm"></a>
+                  <div class="ftco-media-details">
+                          <h3>Mobiliario Oficina.</h3>
+                          <p>Caracas - Venezuela</p>
+                    <!--<strong>$20,000,000</strong>-->
+                  </div>
+                </div> 
+              </div>
+            </div>
 
-
+          </div>
         </div>
       </div>
-    </div>
-    <!-- Fin Seccion Materia Prima-->
+  </div>
+
+  
 
     <!-- Seccion Metodologia-->
     <section class="py-5 bg-pri site-section how-it-works" id="howitworks-section">
@@ -561,6 +487,7 @@
         </div>
       </div>
     </section>
+    -->
     <!-- Fin Seccion Equipo-->
 
     <!-- Seccion Metodologia
@@ -601,6 +528,7 @@
         </div>
       </div>  
     </section>
+    -->
      <!-- Fin Seccion Metodologia-->
 
      <!-- Seccion Nosotros
@@ -629,6 +557,7 @@
         </div>
       </div>
     </section>
+    -->
     <!--Fin  Seccion Nosotros-->
     
          
@@ -671,6 +600,7 @@
         </div>
       </div>
     </section>
+    -->
     <!--Fin Seccion Noticias-->
    
 
@@ -826,19 +756,4 @@
 
   <a href="#top" class="gototop"><span class="icon-angle-double-up"></span></a> 
 
-  <script src="js/jquery-3.3.1.min.js"></script>
-  <script src="js/jquery-ui.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/owl.carousel.min.js"></script>
-  <script src="js/jquery.countdown.min.js"></script>
-  <script src="js/bootstrap-datepicker.min.js"></script>
-  <script src="js/jquery.easing.1.3.js"></script>
-  <script src="js/aos.js"></script>
-  <script src="js/jquery.fancybox.min.js"></script>
-  <script src="js/jquery.sticky.js"></script>
-  
-  <script src="js/main.js"></script>
-    
-  </body>
-</html>
+@endsection
