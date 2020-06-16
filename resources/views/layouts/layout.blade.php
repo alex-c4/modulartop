15e565b77e1ffe4da0644e9a81aae43f249132a8
Alex Penaloza <alexdaniel2601@gmail.com>
2020-06-16 08:09:49 -0400

    se quita link repetido

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -70,11 +70,7 @@
                 <li><a href="{{ route('welcome')}}#services-section" class="nav-link">Servicios</a></li>
                 <li><a href="{{ url('/modulartop') }}" class="nav-link">Modular Top</a></li>
                 <li><a href="{{ url('/novedades') }}" class="nav-link">Novedades</a></li>
-<<<<<<< HEAD
                 <li><a href="{{ route('welcome')}}#contact-section" class="nav-link">Contactanos</a></li>
-=======
-                <li><a href="{{ route('contact.tellus') }}#contact-section" class="nav-link">Contactanos</a></li>
->>>>>>> ea21be69b12eb8a5cb575c97802bd0e2cc11a99f
               </ul>
             </nav>
           </div>
