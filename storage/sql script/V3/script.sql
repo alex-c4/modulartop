8a2fd11225b11ea19f0a0488e6f28b37b332a527
Alex Penaloza <alexdaniel2601@gmail.com>
2020-06-28 15:54:47 -0400

    se actualiza script sql con nuevas categorias

--- storage/sql script/V3/script.sql
+++ storage/sql script/V3/script.sql
@@ -2,3 +2,11 @@ ALTER TABLE `contacts` ADD `name_file` VARCHAR(100) NULL AFTER `message`;
 
 ALTER TABLE `users` ADD `roll_id` INT NOT NULL DEFAULT '2' COMMENT '2 es el valor por defecto, Rol Guest' AFTER `password`;
 
+INSERT INTO `categories` (`id`, `name`, `isDeleted`, `created_at`, `updated_at`) 
+VALUES 
+(NULL, 'Decoración', '0', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
+(NULL, 'Diseño de interiores', '0', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
+(NULL, 'Servicios', '0', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
+(NULL, 'Tipos de muebles', '0', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
+(NULL, 'Historia del mueble', '0', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), 
+(NULL, 'Tendencia', '0', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
