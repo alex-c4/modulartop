6da9f480a3a5b80ee0dc9310138c853180bd13c5
Alex Penaloza <alexdaniel2601@gmail.com>
2025-01-28 19:28:47 -0400

    Enlace para la zona de contacto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -121,7 +121,7 @@ seccionado, mecanizado de madera, prensado mdp, enchapado de tapa cantos" />
       
 
     <!--Seccion contactanos-->
-  <section class="py-5 bg-contact site-section how-it-works" id="howitworks-section">
+  <section class="py-5 bg-contact site-section how-it-works" id='contact-section'>
     <div class="container-contact-info">
       <!-- Contact info section -->
       <div class="contact-info">
@@ -173,7 +173,7 @@ seccionado, mecanizado de madera, prensado mdp, enchapado de tapa cantos" />
       <!-- End WhatsApp section -->
 
       <!-- Email section -->
-      <div class="email-container" id=contact-section>
+      <div class="email-container" >
         <div class="icon-email">
           <svg xmlns="http://www.w3.org/2000/svg" height="100" viewBox="0 0 512 512"><path fill="#787878" d="M64 112c-8.8 0-16 7.2-16 16l0 22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1l0-22.1c0-8.8-7.2-16-16-16L64 112zM48 212.2L48 384c0 8.8 7.2 16 16 16l384 0c8.8 0 16-7.2 16-16l0-171.8L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64l384 0c35.3 0 64 28.7 64 64l0 256c0 35.3-28.7 64-64 64L64 448c-35.3 0-64-28.7-64-64L0 128z"/></svg>
         </div>
